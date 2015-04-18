9bb0ba6d3f64036aee358980932b6743e088006d
Зименко Роман <zimennik@gmail.com>
2015-04-18 06:33:05 +0300

    TabulaRasa

---
+++ Assets/Scripts/GroundMoving.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundMoving : MonoBehaviour
+{
+
+
+    public float speed;
+    public bool isGroundMoving = true;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isGroundMoving)
+        {
+            var temp = GetComponent<Renderer>().material.mainTextureOffset;
+            temp.x += speed*Time.deltaTime;
+            GetComponent<Renderer>().material.mainTextureOffset = temp;
+        }
+    }
+}
